9f14507413d70234546a909916d154324c954634
itzelborbonio <itzelborbonio@gmail.com>
2025-08-05 20:51:19 -0600

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('php/conecta.php');
-$result = $mysqli->query("SELECT * FROM categorias");
-echo "<h1>Categorías</h1><ul>";
-while ($row = $result->fetch_assoc()) {
-    echo "<li><a href='categorias.php?id=" . $row['id_categoria'] . "'>" . $row['nombre'] . "</a></li>";
-}
-echo "</ul>";
-?>
