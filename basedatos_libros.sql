e2a7894bf4419ad40e73a0248e8aa23b0ce0faa1
Víctor Gonzalo Rojas Ledezma <victorgonzalorojasledezma@gmail.com>
2025-08-11 17:15:45 -0600

    Update basedatos_libros.sql
    Añadir "NOT NULL" después del UNIQUE en el campo correo en la tabla usuarios

--- basedatos_libros.sql
+++ basedatos_libros.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS tienda_libros;
 USE tienda_libros;
-CREATE TABLE usuarios (id_usuario INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(100), correo VARCHAR(100) UNIQUE, contraseña VARCHAR(255), direccion TEXT, telefono VARCHAR(20), fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE usuarios (id_usuario INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(100), correo VARCHAR(100) UNIQUE NOT NULL, contraseña VARCHAR(255), direccion TEXT, telefono VARCHAR(20), fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP);
 CREATE TABLE categorias (id_categoria INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(50));
 CREATE TABLE libros (id_libro INT AUTO_INCREMENT PRIMARY KEY, titulo VARCHAR(200), autor VARCHAR(150), editorial VARCHAR(150), descripcion TEXT, precio DECIMAL(10,2), imagen VARCHAR(255), categoria_id INT, FOREIGN KEY (categoria_id) REFERENCES categorias(id_categoria));
 CREATE TABLE pedidos (id_pedido INT AUTO_INCREMENT PRIMARY KEY, id_usuario INT, fecha_pedido DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario));
